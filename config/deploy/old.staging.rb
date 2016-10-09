86c73c2441c5c96a6d6cd826c1e57f275860cc6b
Tetsuya Kusumoto <tetsuya.kusumoto@gmail.com>
2016-10-09 11:56:25 +0900

    change server port

---
+++ config/deploy/old.staging.rb
@@ -0,0 +1,17 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{vagrant@192.168.33.10}
+role :web, %w{vagrant@192.168.33.10}
+role :db,  %w{vagrant@192.168.33.10}
+
+set :rails_env, :staging
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p451'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
--- config/deploy/vagrant.rb
+++
@@ -1,3 +0,0 @@
-role :app, %w{vagrant@vagrant.local}
-role :web, %w{vagrant@vagrant.local}
-role :db,  %w{vagrant@vagrant.local}
